b2e345de7bb29972c98c010636601f0ce02671be
PetrJook <94165300+PetrJook@users.noreply.github.com>
2021-11-12 11:19:48 +0300

    Add files via upload

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -31,4 +31,95 @@ $(function() {
   });
   // In the following sections, you'll add code for sentiment analysis and
   // speech synthesis here.
+  $("#text-to-speech").on("click", function(e) {
+  e.preventDefault();
+  var ttsInput = document.getElementById("translation-result").value;
+  var ttsVoice = document.getElementById("select-voice").value;
+  var ttsRequest = { 'text': ttsInput, 'voice': ttsVoice }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("post", "/text-to-speech", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.responseType = "blob";
+  xhr.onload = function(evt){
+    if (xhr.status === 200) {
+      audioBlob = new Blob([xhr.response], {type: "audio/mpeg"});
+      audioURL = URL.createObjectURL(audioBlob);
+      if (audioURL.length > 5){
+        var audio = document.getElementById("audio");
+        var source = document.getElementById("audio-source");
+        source.src = audioURL;
+        audio.load();
+        audio.play();
+      }else{
+        console.log("An error occurred getting and playing the audio.")
+      }
+    }
+  }
+  xhr.send(JSON.stringify(ttsRequest));
+});
+// Automatic voice font selection based on translation output.
+$('select[id="select-language"]').change(function(e) {
+  if ($(this).val() == "ar"){
+    document.getElementById("select-voice").value = "(ar-SA, Naayf)";
+  }
+  if ($(this).val() == "ca"){
+    document.getElementById("select-voice").value = "(ca-ES, HerenaRUS)";
+  }
+  if ($(this).val() == "zh-Hans"){
+    document.getElementById("select-voice").value = "(zh-HK, Tracy, Apollo)";
+  }
+  if ($(this).val() == "zh-Hant"){
+    document.getElementById("select-voice").value = "(zh-HK, Tracy, Apollo)";
+  }
+  if ($(this).val() == "hr"){
+    document.getElementById("select-voice").value = "(hr-HR, Matej)";
+  }
+  if ($(this).val() == "en"){
+    document.getElementById("select-voice").value = "(en-US, Jessa24kRUS)";
+  }
+  if ($(this).val() == "fr"){
+    document.getElementById("select-voice").value = "(fr-FR, HortenseRUS)";
+  }
+  if ($(this).val() == "de"){
+    document.getElementById("select-voice").value = "(de-DE, HeddaRUS)";
+  }
+  if ($(this).val() == "el"){
+    document.getElementById("select-voice").value = "(el-GR, Stefanos)";
+  }
+  if ($(this).val() == "he"){
+    document.getElementById("select-voice").value = "(he-IL, Asaf)";
+  }
+  if ($(this).val() == "hi"){
+    document.getElementById("select-voice").value = "(hi-IN, Kalpana, Apollo)";
+  }
+  if ($(this).val() == "it"){
+    document.getElementById("select-voice").value = "(it-IT, LuciaRUS)";
+  }
+  if ($(this).val() == "ja"){
+    document.getElementById("select-voice").value = "(ja-JP, HarukaRUS)";
+  }
+  if ($(this).val() == "ko"){
+    document.getElementById("select-voice").value = "(ko-KR, HeamiRUS)";
+  }
+  if ($(this).val() == "pt"){
+    document.getElementById("select-voice").value = "(pt-BR, HeloisaRUS)";
+  }
+  if ($(this).val() == "ru"){
+    document.getElementById("select-voice").value = "(ru-RU, EkaterinaRUS)";
+  }
+  if ($(this).val() == "es"){
+    document.getElementById("select-voice").value = "(es-ES, HelenaRUS)";
+  }
+  if ($(this).val() == "th"){
+    document.getElementById("select-voice").value = "(th-TH, Pattara)";
+  }
+  if ($(this).val() == "tr"){
+    document.getElementById("select-voice").value = "(tr-TR, SedaRUS)";
+  }
+  if ($(this).val() == "vi"){
+    document.getElementById("select-voice").value = "(vi-VN, An)";
+  }
+});
+
 })
