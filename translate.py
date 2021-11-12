c2d8f920a7db440818e1c170d287fb0dd1300203
PetrJook <94165300+PetrJook@users.noreply.github.com>
2021-11-12 11:17:20 +0300

    Add files via upload
    Translator v1.0

---
+++ translate.py
@@ -0,0 +1,23 @@
+import os, requests, uuid, json
+subscription_key = 'e31200a5750249228486dc412e7e6dd3'
+location = 'northeurope'
+
+def get_translation(text_input, language_output):
+    base_url = 'https://api.cognitive.microsofttranslator.com'
+    path = '/translate?api-version=3.0'
+    params = '&to=' + language_output
+    constructed_url = base_url + path + params
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{
+        'text' : text_input
+    }]
+    response = requests.post(constructed_url, headers=headers, json=body)
+    return response.json()
+
